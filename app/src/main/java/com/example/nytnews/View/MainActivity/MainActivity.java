bb6670b6d63253419d513fd755af6830d4c43722
asadullah <asadullah03@gmail.com>
2018-05-10 22:24:01 +0500

    commit

--- app/src/main/java/com/example/nytnews/View/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/nytnews/View/MainActivity/MainActivity.java
@@ -7,9 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 import com.example.nytnews.App;
 import com.example.nytnews.DaggerMainScreenComponent;
@@ -44,18 +42,10 @@ public class MainActivity extends AppCompatActivity implements MainScreenContrac
                 .build().inject(this);
 
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void bindViews() {
-        //recyclerViewNews = ((RecyclerView) findViewById(R.id.));
-        fab = findViewById(R.id.fab);
+        recyclerViewNews =  findViewById(R.id.listofnews_recyclerview);
         toolbar = findViewById(R.id.toolbar);
     }
 
